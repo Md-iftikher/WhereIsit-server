e158bb2415b8f82583b19692440e1885508ccf80
Md-iftikher <iftikherazam@gmail.com>
2025-02-17 21:51:56 +0600

    Project completed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -195,14 +197,13 @@ async function run() {
     });
 
     // Connect the client to the server
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
-    // You can add your routes and database operations here
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
